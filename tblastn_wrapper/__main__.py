995d2467176024e2d8bd3e293d115b379fbb9ce5
Yasir Kusay <yasirsaad1234@hotmail.com>
2020-09-04 14:31:07 +1000

    made recommended changes

--- tblastn_wrapper/__main__.py
+++ tblastn_wrapper/__main__.py
@@ -1,16 +1,18 @@
 import argparse 
+from os import cpu_count
 
 from tblastn_wrapper import tblastn_wrapper
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Find ultraconserved elements across multiple genomes"
+        description="Run multiple tblastn commands at the same time"
     )
 
     parser.add_argument(
         "-query", 
         metavar="QUERY", 
         type=str, 
+        required=True,
         help="the sequence that you want to search"
     )
 
@@ -19,6 +21,14 @@ def parse_args():
         metavar="OUT", 
         type=str, 
         help="the output location"
+    )
+
+    parser.add_argument(
+        "-threads",
+        metavar="THREADS",
+        type=int,
+        help="The number of threads to use",
+        default=cpu_count(),
     )
 
     return parser.parse_known_args()
